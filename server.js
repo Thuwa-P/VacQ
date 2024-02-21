2fc1bcef04f5626054e538bff86c218d0b3f7afb
Thuwa Ngeonsalung <pai.tuva@gmail.com>
2024-02-21 17:55:03 +0700

    Add Authentication

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db.js");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to DB
+connectDB();
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+const hospitals = require("./routes/hospitals.js");
+const auth = require("./routes/auth.js");
+
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log("Server running in", process.env.NODE_ENV, " mode on port ", PORT)
+);
+
+//Handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
